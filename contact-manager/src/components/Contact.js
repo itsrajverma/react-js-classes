fac58a534635210eb277b71073f36c1719c8bc1e
Raj Verma <info.mrverma@gmail.com>
2020-11-02 07:36:22 +0530

    update Imp Contact and delete contact

--- contact-manager/src/components/Contact.js
+++ contact-manager/src/components/Contact.js
@@ -1,23 +1,51 @@
-import React,{ useContext } from "react";
-import { Row,Col } from "reactstrap"
-import { FaRegStar,FaStar } from "react-icons/fa";
-import { MdEdit,MdDelete } from "react-icons/md";
+import React from "react";
+import {Col, Row} from "reactstrap"
+import {FaRegStar, FaStar} from "react-icons/fa";
+import {MdDelete, MdEdit} from "react-icons/md";
 
-const Contact = ({contact,contactKey}) => {
-    return(
+import firebase from "firebase/app";
+import {toast} from "react-toastify";
+
+
+const Contact = ({contact, contactKey}) => {
+
+    const deleteContact = () => {
+        firebase
+            .database()
+            .ref(`/contacts/${contactKey}`)
+            .remove()
+            .then(() => {
+                toast("Deleted Successfully...", {type: "success"});
+            }).catch(err => console.log(err))
+    }
+
+    const updateImpContact = () => {
+        firebase
+            .database()
+            .ref(`/contacts/${contactKey}`)
+            .update({
+                star: !contact.star
+            })
+            .then(() => {
+                toast("Updated Successfully...", {type: "info"});
+            }).catch(err => console.log(err))
+    }
+
+
+    return (
         <Row>
             <Col md={1} className="d-flex justify-content-center align-items-center">
-                <div className="icon">
-                    { contact.star ? (
+                <div className="icon" onClick={()=>updateImpContact()}>
+                    {contact.star ? (
                         <FaStar className="text-primary"/>
                     ) : (
                         <FaRegStar className="text-warning"/>
-                    ) }
+                    )}
                 </div>
             </Col>
 
             <Col md={2} className="d-flex justify-content-center align-items-center">
-                <img src={contact.picture} alt="" className="img-circle profile" />
+                <img src={contact.picture} alt="" className="img-circle profile"/>
             </Col>
 
             <Col md={8}>
@@ -27,9 +55,9 @@ const Contact = ({contact,contactKey}) => {
                 <div className="text-info">{contact.address}</div>
             </Col>
 
-            <Col md={1}  className="d-flex justify-content-center align-items-center">
-                <MdDelete color="danger" className="text-danger icon" />
-                <MdEdit color="info" className="text-info icon ml-2" />
+            <Col md={1} className="d-flex justify-content-center align-items-center">
+                <MdDelete onClick={()=> deleteContact() } color="danger" className="text-danger icon"/>
+                <MdEdit color="info" className="text-info icon ml-2"/>
             </Col>
 
         </Row>
